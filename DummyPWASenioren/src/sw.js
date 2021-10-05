eb37ab4da09c464d2f66473abdebfb1610cf40b0
T. Parmentier <66419209+TParm@users.noreply.github.com>
2021-10-05 11:48:49 +0200

    Delete DummyPWASenioren directory

--- DummyPWASenioren/src/sw.js
+++
@@ -1,4 +0,0 @@
-import {precacheAndRoute} from 'workbox-precaching';
-// Optional: use the injectManifest mode of one of the Workbox
-// build tools to precache a list of URLs, including fallbacks.
-precacheAndRoute(self.__WB_MANIFEST);
